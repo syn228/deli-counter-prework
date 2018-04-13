6530a45b9138968759c6360dbeef282ea17ba9ed
Aaron Na <syn228@nyu.edu>
2018-04-13 19:44:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli" 
+  puts "Welcome, #{name}. You are number #{katz_deli.length" 
 end
   
 def now_serving(name)
