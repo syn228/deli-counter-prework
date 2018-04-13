df3f9c58db5430fd06724eaaeed9646c77d80203
Aaron Na <syn228@nyu.edu>
2018-04-13 19:48:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(name)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  end
+  else
   counter = 0
   emptyArray = katz_deli.push(name)  
     while counter < emptyArray.length
