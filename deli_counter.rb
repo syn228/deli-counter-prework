a1b7a3820194f47ddc93a8ab25dd65d4a5b75b33
Aaron Na <syn228@nyu.edu>
2018-04-13 19:30:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli = []
+
+def line(name)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  end
+  counter = 0
+  emptyArray = katz_deli.push(name)  
+    while counter < emptyArray.length
+    puts "The line is currently: #{counter +1}. #{emptyArray[counter]}"
+    counter += 1
+  end
+end
+
+def take_a_number(katz_deli, name)
+  emptyArray = katz_deli.push(name)
+  counter = 0
+  while counter < emptyArray.length
+  puts emptyArray.push("Welcome, #{name}. You are number #{emptyArray.length} in line.")
+  counter += 1
+end
+end
+  
