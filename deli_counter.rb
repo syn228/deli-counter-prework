ba54e80a4d832e01227064d0ef52fafb32f184a9
Aaron Na <syn228@nyu.edu>
2018-04-13 19:45:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,8 +17,8 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line." 
 end
   
-def now_serving(name)
-  if name.length == 0
+def now_serving(katz_deli)
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   end
   emptyArray = katz_deli.shift(name)
