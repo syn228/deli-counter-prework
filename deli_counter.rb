aa240ad291e02fd82ab1c691ad4282d0691053f2
Aaron Na <syn228@nyu.edu>
2018-04-13 19:35:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,3 +21,16 @@ def take_a_number(katz_deli, name)
 end
 end
   
+def now_serving(name)
+  if katz_deli.length = 0
+    puts "There is nobody waiting to be served!"
+  end
+  emptyArray = katz_deli.shift(name)
+  counter = 0
+  while counter < emptyArray.length
+  puts "Currently serving #{emptyArray[counter]}"
+  counter += 1
+end
+  
+  
+end
