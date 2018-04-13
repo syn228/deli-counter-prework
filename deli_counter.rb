71e631020734d1f83fe391cf9dd296b217ff0a90
Aaron Na <syn228@nyu.edu>
2018-04-13 19:46:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,11 +20,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
-  end
-  emptyArray = katz_deli.shift(name)
-  counter = 0
-  while counter < emptyArray.length
-  puts "Currently serving #{emptyArray[counter]}"
+  elseputs "Currently serving #{emptyArray[counter]}"
   counter += 1
 end
   
