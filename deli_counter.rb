11661285b1457f626e7cbd4163935f9626842bfd
Aaron Na <syn228@nyu.edu>
2018-04-13 19:49:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(name)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else 
-    curretLine = "The line is currently:"
+    currentLine = "The line is currently:"
   katz_deli.each_with_index do |name, index|
-    
+    currentLine = 
     
   end
 end
